32858c797d0fd35c9680901acab81ec3ca5ee276
SchwartzCode <jonbs98@gmail.com>
2025-03-09 22:15:31 -0400

    unify colors more

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -72,18 +72,15 @@ export default function Page() {
     <SidebarProvider>
       <AppSidebar />
       <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+        <header className="flex h-16 shrink-0 items-center gap-2 px-4">
           <SidebarTrigger className="-ml-1" />
           <Separator orientation="vertical" className="mr-2 h-4" />
           <Breadcrumb>
             <BreadcrumbList>
-              <BreadcrumbItem>
-                <BreadcrumbPage>Data Fetching</BreadcrumbPage>
-              </BreadcrumbItem>
             </BreadcrumbList>
           </Breadcrumb>
         </header>
-        <div className="flex flex-1 flex-col gap-4 p-4">
+        <div className="flex flex-1 flex-col gap-4 p-4 bg-spotify-darker-gray">
           <div className="flex flex-col items-center space-y-6">
             <Card>
               <CardHeader>
